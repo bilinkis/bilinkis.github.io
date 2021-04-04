6baeb7f388d2b0a3f058d665d5658d4fd5a34d30
Nicolas Bilinkis <nico@bilinkis.com>
2021-04-04 12:03:37 -0300

    form de login listo

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 let controller = {
     login: function (req,res){
-        res.render('login', {title: 'Página de login'});
+        res.render('login', {title: 'Página de login', path: req.originalUrl});
     }, 
     register: function (req,res){
         res.render('register', {title: 'Página de register', path : req.originalUrl});
