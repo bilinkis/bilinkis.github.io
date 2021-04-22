a9ae54ef6eb81ad19d4899314d74461a22fc34c8
Nico Bilinkis <nico@bilinkis.com>
2021-04-22 19:08:02 -0300

    cambios a rutas y funciones de perfil

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,8 +3,11 @@ let controller = {
         res.render('profile', {title: 'Página del usuario'});
     },
     edit:function (req,res){
-        res.render('profile-edit', {title: 'Página de edición', path: req.originalUrl})
-    }
+        res.render('profile-edit-email', {title: 'Cambiá tu email', path: req.originalUrl});
+    },
+    editPassword: function (req,res){
+        res.render('profile-edit-password',{title: 'Cambiá tu contraseña', path: req.originalUrl})
 
+}
 }
 module.exports = controller;
--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 let profileController = require('../controllers/profileController');
 
 router.get('/', profileController.main);
-router.get('/edit', profileController.edit);
+router.get('/edit-email', profileController.edit);
+router.get('/edit-password', profileController.editPassword);
 
 module.exports = router;
