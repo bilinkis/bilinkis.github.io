0b0eb13e096631bd0d1dd24f157c29c465c337a9
Nicolas Bilinkis <nico@bilinkis.com>
2021-05-10 16:29:49 -0400

    detalle de producto con datos dinamicos

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,55 @@
+const db = require("../database/models");
+const Op = db.Sequelize.Op;
+
 let controller = {
     main: function (req,res){
-        return res.render('product', {title: 'Página de productos'});
+        let posts = new Promise(function(resolve,reject){
+            db.Posts.findByPk(req.params.id)
+        .then(function(data){
+            resolve(data);
+            
+        })
+        .catch(function(err){
+            reject();
+            console.log(err);
+        })
+        })
+        let comments = new Promise(function(resolve,reject){
+            db.Comments.findAll({
+                raw:true,
+                where:{
+                    productId: req.params.id,
+                }
+            })
+            .then(function(data){
+                
+                resolve(data);
+            })
+            .catch(function(err){
+                reject();
+                console.log(err)
+            })
+        })
+        
+        Promise.all([posts,comments])
+        .then(function(values){
+            return res.render('product', {title:"Detalle de producto", posts:values[0], comments:values[1]})
+        })
+            
+                
+            .catch(function(err){
+                console.log(err)
+            })
+                
+                
+                
+                    
+                
+            
+        
+
+                
+         
     }, 
     add: function (req,res){
         return res.render('product-add', {title: 'Agregar producto', path : req.originalUrl});
