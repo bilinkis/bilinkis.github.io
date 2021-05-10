e836e8dc8d6fb25290e38e05badaba1979489957
Nicolas Bilinkis <nico@bilinkis.com>
2021-05-10 11:59:27 -0400

    home con productos dinámicos

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,42 @@
+const db = require("../database/models");
+const Op = db.Sequelize.Op;
 let controller = {
     main: function (req, res){
-        return res.render('index', {title: 'Index'});
+        let findLatestProducts = new Promise(function(resolve,reject){
+            db.Posts.findAll({
+            limit: 8,
+            order: [ [ 'id', 'DESC' ]]
+    })
+            .then(function(data){
+            resolve(data);
+            })
+            .catch(function(err){
+                reject();
+                console.log(err);
+            })
+        })
+        let findMostCommented = new Promise(function(resolve,reject){
+            db.Posts.findAll({
+                raw:true,
+                limit:8,
+            })
+            .then(function(data){
+                resolve(data);
+            })
+            .catch(function(err){
+                reject();
+                console.log(err);
+            })
+        })
+        Promise.all([findLatestProducts,findMostCommented])
+        .then(function(values){
+            console.log(values[0].title)
+            return res.render('index', {title: 'Home', latestProducts:values[0], mostCommented:values[1]});
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+        
     }
 }
 module.exports = controller;
