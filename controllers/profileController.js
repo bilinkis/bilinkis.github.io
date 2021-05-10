fe9a83712ec3565e7303d32e6f8931f431976fc7
Nicolas Bilinkis <nico@bilinkis.com>
2021-05-10 11:19:28 -0400

    perfiles dinamicos según el ID

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,57 @@
+const db = require("../database/models");
+const Op = db.Sequelize.Op;
+
 let controller = {
     main: function (req,res){
-        return res.render('profile', {title: 'Página del usuario'});
+    let findUser = new Promise(function(resolve,reject){
+        db.Users.findByPk(req.params.id)
+        .then(function(data){
+            
+            resolve(data);
+        })
+        .catch(function(err){
+            reject();
+        })
+    })
+    let findPosts = new Promise(function(resolve,reject){
+        db.Posts.findAll({
+            raw:true,
+            where:{
+                userId:req.params.id,
+            }
+        })
+        .then(function(dataPosts){
+            resolve(dataPosts);
+            
+        })
+
+        .catch(function(err){
+            reject()
+        })
+    })
+    let findComments = new Promise(function(resolve,reject){
+        db.Comments.findAll({
+            raw:true,
+            where: {
+                userId:req.params.id
+            }
+        })
+        .then(function(data){
+            resolve(data);
+        })
+        .catch(function(err){
+            console.log(err);
+            reject()
+        })
+    })
+    Promise.all([findUser,findPosts,findComments])
+    .then(function(values){
+        console.log();
+        return res.render('profile', {title:"Perfil", user:values[0].dataValues, posts:values[1], comments:values[2]})
+    })
+    .catch(function(err){
+        console.log(err);
+    })
     },
     edit:function (req,res){
         return res.render('profile-edit-email', {title: 'Cambiá tu email', path: req.originalUrl});
--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 let profileController = require('../controllers/profileController');
 
-router.get('/', profileController.main);
+router.get('/:id', profileController.main);
 router.get('/edit-email', profileController.edit);
 router.get('/edit-password', profileController.editPassword);
 
