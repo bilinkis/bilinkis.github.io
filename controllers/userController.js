1524ae5964329f1811bb38bd4749fecca07c0d63
Nicolas Bilinkis <nico@bilinkis.com>
2021-05-18 08:13:12 -0400

    nuevas validaciones y alertas en login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,108 +1,111 @@
 const db = require("../database/models");
 const Op = db.Sequelize.Op;
 const bcrypt = require("bcryptjs");
-var multer = require('multer');
-var fs = require('fs');
-var path = require('path');
-var storage =   multer.diskStorage({
+var multer = require("multer");
+var fs = require("fs");
+var path = require("path");
+var storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    
-            callback(null, 'public/images/users');
-        },
+    callback(null, "public/images/users");
+  },
   filename: function (req, file, callback) {
-    callback(null,  'user-' + Date.now() + path.extname(file.originalname));
-  }
+    callback(null, "user-" + Date.now() + path.extname(file.originalname));
+  },
 });
-var upload = multer({ storage : storage}).single('user_file');
+var upload = multer({ storage: storage }).single("user_file");
 let controller = {
-    viewLogin: function (req,res){
-        if(res.locals.loggedIn == null){
-        return res.render('login', {title: 'Página de login', path: req.originalUrl});
+  viewLogin: function (req, res) {
+    if(res.locals.loggedIn == null){
+      return res.render("login", {
+        title: "Página de login",
+        path: req.originalUrl,
+        error: req.cookies.error,
+      });
     } else{
-        return res.redirect('/profile/'+res.locals.user.id)
+        res.redirect('/profile/'+res.locals.user.id)
     }
-    }, 
-    viewRegister: function (req,res){
-        if(res.locals.loggedIn == null){
-            return res.render('register', {title: 'Página de register', path : req.originalUrl});
-        } else{
-            return res.redirect('/profile/'+res.locals.user.id)
-        }
-        
-    },
-    login:function(req,res){
-        
-        let data= req.body;
-        
-        let hashPassword = bcrypt.hashSync(req.body.user_password, 10)
-        
-        db.Users.findOne({
-            raw:true,
-            where:{
-                email: data.user_email,
-                
-            }
-        })
-        .then(function(data){
-            console.log(data)
-            if(data != null){
-            let checkPass = bcrypt.compareSync(req.body.user_password,data.password);
-            if(checkPass = true){
-                req.session.user = data;
-                req.session.loggedIn = true;
-                req.session.save();
-                res.redirect('/profile/'+data.id)
-                if(req.body.rememberme != undefined){
-                    res.cookie('userId', user.id, { maxAge: 1000 * 60 * 5 });
-                    }
-            }
-            else{
-                console.log(hashPassword)
-                console.log(data.password)
-                res.send(alert('Reescriba su mail y/o contraseña'))
-            }
-            
-        }
-        else{
-            console.log("no hay user");
-        }
-        
-            
-        })
-        .catch(function(err){
-            console.log(err);
-        })
-        
-    },
-    store: function(req,res){
-        
-        
-        
-        upload(req,res,function(err) {
-            
-            if(err) {
-                console.log(err);
-            }
+    
+  },
+  viewRegister: function (req, res) {
+    if (res.locals.loggedIn == null) {
+      return res.render("register", {
+        title: "Página de register",
+        path: req.originalUrl,
+      });
+    } else {
+      return res.redirect("/profile/" + res.locals.user.id);
+    }
+  },
+  login: function (req, res) {
     let data = req.body;
-    let passEncriptada = bcrypt.hashSync(req.body.password, 10)
-        db.Users.create({
-            name: data.name,
-            lastName: data.lastName,
-            email: data.email,
-            phone: data.phone,
-            gender: data.gender,
-            password: passEncriptada,
-            birthday: data.birthday,
-            image:req.file.filename
-        });
-        return res.redirect('/')
+
+    let hashPassword = bcrypt.hashSync(req.body.user_password, 10);
+
+    db.Users.findOne({
+      raw: true,
+      where: {
+        email: data.user_email,
+      },
     })
-    },
-    logout: function(req,res){
-        req.session.destroy();
-        res.clearCookie('userId');
-        res.clearCookie('loggedIn');
-        return res.redirect('/');
-    }
-}
+      .then(function (data) {
+        console.log(data);
+        if (data) {
+            console.log(data);
+          let checkPass = bcrypt.compareSync(
+            req.body.user_password,
+            data.password
+          );
+          console.log("pass:"+checkPass);
+          if (checkPass == true) {
+            if (req.body.rememberme != undefined) {
+                res.cookie("userId", user.id, { maxAge: 1000 * 60 * 5 });
+              }
+            
+            req.session.user = data;
+            req.session.loggedIn = true;
+            req.session.save();
+            
+            return res.redirect("/profile/" + data.id);
+          } else {
+            
+            res.cookie("error", "nopassword", {maxAge:1000});
+            return res.redirect("/login");
+          }
+        } else {
+          console.log("no hay email");
+          res.cookie("error", "noemail", {maxAge:1000});
+          return res.redirect("/login");
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  },
+  store: function (req, res) {
+    upload(req, res, function (err) {
+      if (err) {
+        console.log(err);
+      }
+      let data = req.body;
+      let passEncriptada = bcrypt.hashSync(req.body.password, 10);
+      db.Users.create({
+        name: data.name,
+        lastName: data.lastName,
+        email: data.email,
+        phone: data.phone,
+        gender: data.gender,
+        password: passEncriptada,
+        birthday: data.birthday,
+        image: req.file.filename,
+      });
+      return res.redirect("/login");
+    });
+  },
+  logout: function (req, res) {
+    req.session.destroy();
+    res.clearCookie("userId");
+    res.clearCookie("loggedIn");
+    return res.redirect("/");
+  },
+};
 module.exports = controller;
