7adc118488e3b4c17c53c3c4db3595024de4805a
Josefina <jstaudenmaier@udesa.edu.ar>
2021-05-25 18:46:47 -0300

    ejs ranking + db columnas en users

--- controllers/rankingController.js
+++ controllers/rankingController.js
@@ -2,6 +2,8 @@ const db = require("../database/models");
 const Op = db.Sequelize.Op;
 
 let controller = {
+    main: function(req, res){
+        
+    }
 
-    
 }
--- data/database.sql
+++ data/database.sql
@@ -7,7 +7,11 @@ phone INT NOT NULL,
 gender VARCHAR(100) NOT NULL, 
 password VARCHAR(64) NOT NULL,
 birthday DATE NOT NULL,
-image VARCHAR(100)
+image VARCHAR(100),
+followers INT UNSIGNED,
+following INT UNSIGNED,
+commentsPosted INT UNSIGNED,
+commentsReceived INT UNSIGNED
 );
 
 CREATE TABLE posts (
@@ -32,16 +36,9 @@ productId INT UNSIGNED,
 FOREIGN KEY (userId) REFERENCES users(id),
 FOREIGN KEY (productId) REFERENCES posts(id)
 );
-CREATE TABLE followers (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    followed INT UNSIGNED,
-    follower INT UNSIGNED,
-    FOREIGN KEY (followed) REFERENCES users(id),
-    FOREIGN KEY (follower) REFERENCES users(id)
-);
 
 INSERT INTO users
-VALUES (DEFAULT, 'Bautista', 'Abella', 'bautiabella@hotmail.com', '123345678', 'Prefiero no responder', 'bauticapo123', '2001-08-23', ''),(DEFAULT, 'Sofia', 'Ruiz', 'sofiruiz12@gmail.com', '123345678', 'Mujer', 'sofitalinda89', '2003-02-06',''), (DEFAULT, 'Josefina', 'Staudenmaier', 'jochust@gmail.com', '1154701208', 'Mujer', 'cangurito23', '2001-08-12',''), (DEFAULT, 'Nicolas', 'Bilinkis', 'nico@bilinkis.com', '12345648', 'Hombre', 'amogithub', '1999-08-03',''), (DEFAULT, 'Carlos', 'Perez', 'charliperez@hotmail.com', '1554789699', 'Hombre', 'charlikari', '1970-11-25','');
+VALUES (DEFAULT, 'Bautista', 'Abella', 'bautiabella@hotmail.com', '123345678', 'Prefiero no responder', 'bauticapo123', '2001-08-23','', 0, 0, 0, 0),(DEFAULT, 'Sofia', 'Ruiz', 'sofiruiz12@gmail.com', '123345678', 'Mujer', 'sofitalinda89', '2003-02-06','', 0, 0, 0, 0), (DEFAULT, 'Josefina', 'Staudenmaier', 'jochust@gmail.com', '1154701208', 'Mujer', 'cangurito23', '2001-08-12','', 0, 0, 0, 0), (DEFAULT, 'Nicolas', 'Bilinkis', 'nico@bilinkis.com', '12345648', 'Hombre', 'amogithub', '1999-08-03','', 0, 0, 0, 0), (DEFAULT, 'Carlos', 'Perez', 'charliperez@hotmail.com', '1554789699', 'Hombre', 'charlikari', '1970-11-25','', 0, 0, 0, 0);
 
 INSERT INTO posts
 VALUES (DEFAULT, 'Teg Clásico', 'Disfrutá de una apasionante acción bélica donde intervienen la lógica, la inteligencia y el azar.El juego tiene lugar en un planisferio dividido en 50 países. Cada jugador tiene un objetivo secreto a cumplir, para lo cual deberá, mediante diferentes estrategias, ampliar sus dominios, reordenar sus fuerzas, realizar pactos, emprender ataques y defenderse de los adversarios. El primer jugador en lograr su objetivo secreto, o conquistar 30 países (objetivo común), será el ganador.', '1.jpg', '1', DEFAULT,0),
@@ -55,10 +52,10 @@ VALUES (DEFAULT, 'Teg Clásico', 'Disfrutá de una apasionante acción bélica d
 (DEFAULT, 'Jenga', 'Se trata de una estructura compuesta por varios bloques de madera, y el objetivo es ir quitando estos bloques sin que se desmonte la estructura.', '9.jpg', '2', DEFAULT,0),
 (DEFAULT, 'Juego de Tronos', 'Es un juego de mesa muy completo con un poco de todo: rol, comercio, economía, estrategia, cartas, PvP y PvE… Lo recomiendo sobretodo para gente que quiera dar un salto de aficionado a algo más complejo y pro.', '10.jpg', '1', DEFAULT,0);
 
-SELECT *FROM users;
-
-
 INSERT INTO comments
 VALUES (DEFAULT,'1','Muy buen producto!',DEFAULT,'1'),(DEFAULT,'2','Gran producto!',DEFAULT,'2'),(DEFAULT,'3','Recomendado',DEFAULT,'3'),(DEFAULT,'4','Muy buen producto!',DEFAULT,'4'),(DEFAULT,'5','No está bueno!',DEFAULT,'5'),(DEFAULT,'5','Buen producto!',DEFAULT,'6'),(DEFAULT,'1','No me gustó',DEFAULT,'7'),(DEFAULT,'2','Malisimo!',DEFAULT,'8'),(DEFAULT,'3','Muy buen producto!',DEFAULT,'9'),(DEFAULT,'4','Buenisimo',DEFAULT,'10'),(DEFAULT,'3','Muy buen producto!',DEFAULT,'1'),(DEFAULT,'2','Muy buen producto!',DEFAULT,'2'),(DEFAULT,'4','buen producto!',DEFAULT,'3'),(DEFAULT,'5','pésimo!',DEFAULT,'4'),(DEFAULT,'1','Muy buen producto!',DEFAULT,'5'),(DEFAULT,'5','terrible producto!',DEFAULT,'6'),(DEFAULT,'2','muy conforme',DEFAULT,'7'),(DEFAULT,'1','No me gustó para nada',DEFAULT,'8'),(DEFAULT,'4','Muy buen producto!',DEFAULT,'9'),(DEFAULT,'1','Recomendado para mayores de 5',DEFAULT,'10'),(DEFAULT,'2','buen producto',DEFAULT,'1'),(DEFAULT,'3','me gustó',DEFAULT,'2'),(DEFAULT,'4','tremendo!',DEFAULT,'3'),(DEFAULT,'1','malisimo',DEFAULT,'4'),(DEFAULT,'4','re bueno',DEFAULT,'5'),(DEFAULT,'2','recomendable para todos',DEFAULT,'6'),(DEFAULT,'5','a mi mamá no le gustó',DEFAULT,'7'),(DEFAULT,'1','buen regalo',DEFAULT,'8'),(DEFAULT,'5','Super divertido',DEFAULT,'9'),(DEFAULT,'3','buenisimo',DEFAULT,'10'),(DEFAULT,'1','Muy buen producto!',DEFAULT,'1'),(DEFAULT,'2','buen producto!',DEFAULT,'2'),(DEFAULT,'3','flojo producto!',DEFAULT,'3'),(DEFAULT,'4','muy malo',DEFAULT,'4'),(DEFAULT,'5','compraría de nuevo!',DEFAULT,'5'),(DEFAULT,'1','no volvería a comprar',DEFAULT,'6'),(DEFAULT,'2','se lo regale a mi hijo y no le gusto',DEFAULT,'7'),(DEFAULT,'3','me encantó!',DEFAULT,'8'),(DEFAULT,'4','re mil bueno',DEFAULT,'9'),(DEFAULT,'5','muy largas las instrucciones',DEFAULT,'10');
+
+
+
 
 
