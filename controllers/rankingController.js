cf0a5a3adad57cde64bb251ecfdf7b22187c2d83
Josefina <jstaudenmaier@udesa.edu.ar>
2021-05-26 09:19:49 -0300

    ranking controller

--- controllers/rankingController.js
+++ controllers/rankingController.js
@@ -3,7 +3,84 @@ const Op = db.Sequelize.Op;
 
 let controller = {
     main: function(req, res){
-        return res.render('ranking')
+        let mostFollowed = new Promise(function(resolve, reject){
+            db.Users.findAll({
+                raw: true, 
+                limit: 8,
+                order: [ ['followers', 'DESC']],
+            })
+            .then(function(data){
+                resolve(data);
+            })
+            .catch(function(error){
+                reject();
+                console.log(error);
+            })
+        })
+        let following = new Promise(function(resolve, reject){
+            db.Users.findAll({
+                raw: true, 
+                limit: 8,
+                order: [ ['following', 'DESC']],
+            })
+            .then(function(data){
+                resolve(data);
+            })
+            .catch(function(error){
+                reject();
+                console.log(error);
+            })
+        })
+        let mostCommentsPosted = new Promise(function(resolve, reject){
+            db.Users.findAll({
+                raw: true, 
+                limit: 8,
+                order: [ ['commentsPosted', 'DESC']],
+            })
+            .then(function(data){
+                resolve(data);
+            })
+            .catch(function(error){
+                reject();
+                console.log(error);
+            })
+        })
+        let mostCommentsReceived = new Promise(function(resolve, reject){
+            db.Users.findAll({
+                raw: true, 
+                limit: 8,
+                order: [ ['commentsReceived', 'DESC']],
+            })
+            .then(function(data){
+                resolve(data);
+            })
+            .catch(function(error){
+                reject();
+                console.log(error);
+            })
+        })
+        /*let mostPosts = new Promise(function(resolve, reject){
+            db.Posts.findAll({
+                raw: true, 
+                limit: 8,
+                order: [ ['', 'DESC']],
+                include:[{}]
+            })
+            .then(function(data){
+                resolve(data);
+            })
+            .catch(function(error){
+                reject();
+                console.log(error);
+            })
+        })*/
+        Promise.all([mostFollowed, following, mostCommentsPosted, mostCommentsReceived])
+        .then(function(values){
+            return res.render('ranking', {title: 'Ranking de usuarios', mostFollowed:[0], following:[1], mostCommentsPosted:[2], mostCommentsReceived:[3]})
+        })
+        .catch(function(error){
+            console.log(error);
+        })
     }
 
 }
