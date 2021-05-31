5b3be4facc066c5211723d9b92a8ba06dc4582ea
Josefina <jstaudenmaier@udesa.edu.ar>
2021-05-31 14:42:34 -0300

    cambios en ruta profile y ejs commentsR

--- routes/profile.js
+++ routes/profile.js
@@ -8,7 +8,7 @@ router.get('/:id/edit-password', profileController.editPassword);
 router.get('/:id/followers',profileController.followers);
 router.get('/:id/following',profileController.following);
 router.get('/:id/commentsreceived', profileController.commentsReceived);
-router.get('/:id/comments', profileController.commentsReceived);
+router.get('/:id/comments', profileController.comments);
 router.post('/edit-email', profileController.storeEditEmail);
 router.post('/edit-password', profileController.storeEditPassword);
 router.post('/delete', profileController.delete);
